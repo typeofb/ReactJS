283abad12f2a80717ca654645b121bd183730690
typeofb@gmail.com <jordan12>
2020-02-18 10:33:49 +0900

    action, reducer, store 추가

--- web/resources/js/comments.jsx
+++ web/resources/js/comments.jsx
@@ -1,18 +1,23 @@
 class Comment extends React.Component {
   render() {
-    return (
-      <ul>
+    let commentNodes = store.getState().items.map((item) =>
+      <ul key={item.fakeNm}>
         <li>
           <div>
-            <span>아이디</span>
+            <span>아이디 : {item.fakeNm}</span>
           </div>
           <div>
-            <span><p>메시지</p></span>
+            <span><p>메시지 : {item.realNm}</p></span>
             <button>수정</button>
             <button>삭제</button>
           </div>
         </li>
       </ul>
+    );
+    return (
+      <div>
+        {commentNodes}
+      </div>
     );
   }
 }
@@ -21,7 +26,7 @@ class CommentList extends React.Component {
   render() {
     return (
       <div>
-        <Comment></Comment>
+        <Comment/>
         <button>더보기</button>
       </div>
     );
@@ -66,6 +71,68 @@ class CommentBox extends React.Component {
   }
 }
 
-ReactDOM.render(<CommentBox/>,
-  document.getElementById('content')
-);
+// action types
+const SELECT = 'SELECT';
+const INSERT = 'INSERT';
+const UPDATE = 'UPDATE';
+const DELETE = 'DELETE';
+
+// action creators
+function selectComment() {
+  return {type: SELECT};
+}
+
+function insertComment() {
+  return {type: INSERT};
+}
+
+function updateComment() {
+  return {type: UPDATE};
+}
+
+function deleteComment(comment) {
+  return {
+    type: DELETE,
+    payload: comment
+  };
+}
+
+function reducer(state = [], action) {
+  switch (action.type) {
+    case SELECT:
+      return Object.assign({}, state, {fakeNm: state.fakeNm});
+    case INSERT:
+      return Object.assign({}, state, {fakeNm: state.fakeNm});
+    case UPDATE:
+      return Object.assign({}, state, {fakeNm: state.fakeNm});
+    case DELETE:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  items: [
+    {
+      fakeNm: 0,
+      realNm: '홍길동'
+    },
+    {
+      fakeNm: 1,
+      realNm: '콩쥐'
+    },
+    {
+      fakeNm: 2,
+      realNm: '팥쥐'
+    }
+  ]
+};
+const store = Redux.createStore(reducer, initialState);
+const render = () => {
+  ReactDOM.render(<CommentBox store={store}/>,
+    document.getElementById('content')
+  );
+}
+store.subscribe(render);
+render();
