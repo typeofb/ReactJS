35e754375b3d9c76571f4cad391705d33d98ec7f
M53S-G656\typeo <typeofb@gmail.com>
2022-08-03 19:44:46 +0900

    3차원 그래프 표출

---
+++ web/resources/js/chart.js
@@ -0,0 +1,168 @@
+const arr = {
+    "statusCode": 200,
+    "error": "",
+    "count": 56,
+    "data": [
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1001.35286102792", "yValue": 0.503707299980537},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1001.35286102792", "yValue": 0.618073498355432},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1001.35286102792", "yValue": 0.147670285379206},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1001.35286102792", "yValue": 0.262143529101536},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1001.35286102792", "yValue": -1.74471864432083},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1001.35286102792", "yValue": 0.301973221739098},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1001.35286102792", "yValue": 0.999959995152414},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1001.35286102792", "yValue": 0.406402053455002},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1001.92761621991", "yValue": 0.74620491520577},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1001.92761621991", "yValue": 2.08499081105924},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1001.92761621991", "yValue": 0.269103348032761},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1001.92761621991", "yValue": 0.251644278477274},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1001.92761621991", "yValue": 0.109754017085538},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1001.92761621991", "yValue": 0.339482082834245},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1001.92761621991", "yValue": 0.410148373939782},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1001.92761621991", "yValue": 0.458558907660831},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1002.5021732166", "yValue": 0.550313805086447},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1002.5021732166", "yValue": 0.665996253086715},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1002.5021732166", "yValue": 0.178029930439109},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1002.5021732166", "yValue": 0.172119648502066},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1002.5021732166", "yValue": -3.59858671903348},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1002.5021732166", "yValue": 0.283370062518873},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1002.5021732166", "yValue": 0.194148652808101},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1002.5021732166", "yValue": 0.523959650201772},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1003.07653200527", "yValue": 0.399539760950342},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1003.07653200527", "yValue": 0.244871446150228},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1003.07653200527", "yValue": 0.784218869096652},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1003.07653200527", "yValue": 0.201991233947589},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1003.07653200527", "yValue": 0.674451784226768},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1003.07653200527", "yValue": 0.350300512101813},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1003.07653200527", "yValue": 0.164406139233226},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1003.07653200527", "yValue": 0.304813659082228},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1003.6506925732", "yValue": 0.262654846895227},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1003.6506925732", "yValue": 0.307984570883369},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1003.6506925732", "yValue": 0.185493558522309},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1003.6506925732", "yValue": 0.176377018445432},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1003.6506925732", "yValue": 0.443906523932841},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1003.6506925732", "yValue": -10.9237736850425},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1003.6506925732", "yValue": 0.302437799693912},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1003.6506925732", "yValue": 0.196268754557287},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1004.22465490765", "yValue": 0.346329786429587},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1004.22465490765", "yValue": 0.215416350483923},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1004.22465490765", "yValue": 0.163822985738182},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1004.22465490765", "yValue": 0.297011802317214},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1004.22465490765", "yValue": 0.434238132007201},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1004.22465490765", "yValue": 0.378270068880992},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1004.22465490765", "yValue": 0.315758125526686},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1004.22465490765", "yValue": 0.417726815622572},
+        {"time": "2022-07-12T03:00:00Z", "xValue": "1004.79841899591", "yValue": 0.685479640458147},
+        {"time": "2022-07-12T04:00:00Z", "xValue": "1004.79841899591", "yValue": 0.173687751194921},
+        {"time": "2022-07-12T05:00:00Z", "xValue": "1004.79841899591", "yValue": 0.264228111368263},
+        {"time": "2022-07-12T06:00:00Z", "xValue": "1004.79841899591", "yValue": 1.30261658918709},
+        {"time": "2022-07-12T07:00:00Z", "xValue": "1004.79841899591", "yValue": 0.274402762715709},
+        {"time": "2022-07-12T08:00:00Z", "xValue": "1004.79841899591", "yValue": 0.274437302870058},
+        {"time": "2022-07-12T09:00:00Z", "xValue": "1004.79841899591", "yValue": 0.171207886490035},
+        {"time": "2022-07-12T10:00:00Z", "xValue": "1004.79841899591", "yValue": 0.517584677642406}
+    ]
+}
+
+var objArray = [];  // object를 배열화
+var array2;         // time별 x값과 y값
+var timeList = [];  // time의 종류를 배열화
+let dataSet = [];   // 그래프에 표시할 데이터들을 담을 배열
+
+// 1. object를 배열로 전환
+for (var i in arr.data) {
+    objArray.push(arr.data[i]);
+}
+
+// 2. time별로 중복이 없도록 하여 배열 생성
+for (var i = 0; i < objArray.length; i++) {
+    if (timeList[0] == objArray[i].time) {
+        break;
+    }
+    timeList.push(objArray[i].time);
+}
+
+// 3. 2차원 배열 생성
+array2 = new Array(timeList.length);
+for (var i = 0; i < array2.length; i++) {
+    array2[i] = new Array(3);
+}
+
+// 4. time별 xValue와 yValue를 정리
+// 4-1. 스코프를 고려한 배열 초기화 위치
+var xValue = [];
+var yValue = [];
+// 4-2. 같은 time을 가진 x값과 y값울 묶음
+for (var i = 0; i < timeList.length; i++) {
+    for (var j = 0; j < objArray.length; j++) {
+        if (objArray[i].time == objArray[j].time) {
+            xValue.push(objArray[j].xValue)
+            yValue.push(objArray[j].yValue)
+        }
+    }
+    array2[i][0] = timeList[i];
+    array2[i][1] = xValue;
+    array2[i][2] = yValue;
+
+    // 변수 재사용목적 초기화
+    yValue = []
+    if (i != timeList.length - 1) {
+        xValue = []  // x는 항상 같으므로
+    }
+}
+
+const labels = xValue;
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: array2[0][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'red',
+            data: array2[0][2],
+        }, {
+            label: array2[1][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'yellow',
+            data: array2[1][2],
+        }, {
+            label: array2[2][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'pink',
+            data: array2[2][2],
+        }, {
+            label: array2[3][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'blue',
+            data: array2[3][2],
+        }, {
+            label: array2[4][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'green',
+            data: array2[4][2],
+        }, {
+            label: array2[5][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'orange',
+            data: array2[5][2],
+        }, {
+            label: array2[6][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'black',
+            data: array2[6][2],
+        }, {
+            label: array2[7][0].substr(11, 5) + 'AM',
+            pointRadius: 1,
+            borderColor: 'purple',
+            data: array2[7][2],
+        }
+    ]
+};
+
+const config = {
+    type: 'line',
+    data: data,
+    options: {}
+};
+
+const chartID = new Chart(
+    document.getElementById("chartID"), config
+);
